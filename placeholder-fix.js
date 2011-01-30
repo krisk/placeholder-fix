3276fe8bccde5cd60013ae1827bbacb02a0f2abe
krisk <kirollos@gmail.com>
2011-01-30 17:00:17 -0500

    Added fles

---
+++ placeholder-fix.js
@@ -0,0 +1,53 @@
+﻿/*!
+* placeholderFix v1.0
+*
+* Dependencies:
+* - jQuery
+*
+* Copyright 2011, Kirollos Risk
+* Dual licensed under the MIT or GPL Version 2 licenses.
+*
+* Date: Jan 28 10:00:00 2011
+*/
+(function ($) {
+    $(document).ready(function () {
+        // Check if browser supports the placeholder attribute
+        if ('placeholder' in document.createElement('input')) {
+            return;
+        }
+
+        // Create a style sheet with a watermark class
+        $($("head")[0]).append($('<style type="text/css"> textarea.watermark, input.watermark { color: Gray; } </style>'));
+
+        // Find all the elements of input type="text" and textareas
+        $.each($('input[type=text], textarea'), function (index, elem) {
+
+            var elem = $(elem);
+            var placeholder = elem.attr('placeholder');
+
+            if (typeof placeholder !== 'undefined' && placeholder.length > 0) {
+                if (elem.val().length === 0) {
+                    elem
+                        .addClass('watermark')
+                        .val(placeholder);
+                }
+                elem
+                    .focus(function () {
+                        if (this.value == placeholder) {
+                            $(this)
+                                .val('')
+                                .removeClass('watermark');
+
+                        }
+                    })
+                    .blur(function () {
+                        if (this.value.length === 0) {
+                            $(this)
+                                .addClass('watermark')
+                                .val(placeholder);
+                        }
+                    });
+            }
+        });
+    });
+})(jQuery);
---
+++ readme.md
@@ -0,0 +1,4 @@
+Placeholder fix
+---------------
+
+The placeholder attribute has been introduced in HTML5 specifications for input and textarea elements. The placeholder text is shown when the element is empty and not focused.  However, some browsers do not support this attribute.  This fix is a fallback until the placholder attribute is fully supported in *all* browsers.
